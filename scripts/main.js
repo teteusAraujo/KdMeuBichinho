7292ded9da382841de2010d59929779d9367e3e2
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-05 13:06:10 -0300

    Criação do arquivo  main.js

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const BASE_URL_CLIENT = "http://localhost:5500/";
+const BASE_URL_CLIENT = "https://kdmeubichinho.github.io/";
+const BASE_URL_SERVER = "https://kdmeubichinho-app.herokuapp.com/";
+
+const API_ANUNCIO_BUSCA = "anuncio/busca?";
+const API_FOTO = "storage/upload";
+const API_ANUNCIO = "anuncio/";
+const API_MENSAGEM = "mensagem/"
+const API_AUTH = "pessoa/auth"
+const API_PESSOA = "pessoa/"
+const API_ATUALIZA_STATUS = "atualizastatus/"
+const CLIENT_PETPROFILE = "pages/petprofile.html";
+const CLIENT_PETADVERT = "pages/petadvert.html"
+const CLIENT_LOGIN = "pages/login.html"
+
+const URL_BLOCK = [`${BASE_URL_CLIENT}${CLIENT_PETADVERT}`, ]
