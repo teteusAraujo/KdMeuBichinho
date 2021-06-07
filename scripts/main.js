92a7ef8faa9a2432ab871fd6e57cebd45fe05696
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-07 20:30:20 -0300

    Deixando o HTML dinamico da pagina principal

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,20 @@ const CLIENT_PETADVERT = "pages/petadvert.html"
 const CLIENT_LOGIN = "pages/login.html"
 
 const URL_BLOCK = [`${BASE_URL_CLIENT}${CLIENT_PETADVERT}`, ]
+
+let token = false;
+const menu = document.querySelector('#menu')
+const footer = document.querySelector('#footer')
+const localizacaoViaCep = {}
+const menuLogout = `
+    <a href="${BASE_URL_CLIENT}">
+        <img src="${BASE_URL_CLIENT}images/logo-black.svg" alt="Logo KdMeuBichinho" class="logo">
+    </a>
+    <input type="checkbox" id="bar">
+    <label for="bar" class="bars">&#9776;</label>
+    <ul class="">
+        <li><a href="${BASE_URL_CLIENT}">Início <!--<i class="fas fa-home"></i>--></a></li>
+        <li><a href="${BASE_URL_CLIENT}pages/petadvert.html">Anunciar <!--<i class="fas fa-plus">--></i></a></li>
+        <li><a href="${BASE_URL_CLIENT}pages/signup.html">Cadastrar <!--<i class="fas fa-user-plus"></i>--></a></li>
+        <li><button class="btn-primary" onclick="location.href='${BASE_URL_CLIENT}pages/login.html'">Entrar  <!--<i class="fas fa-sign-in-alt">--></i></button></li>
+    </ul>  
