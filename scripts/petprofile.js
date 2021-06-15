19dd3eb8dda78ddd4f022a01699249349d4d4987
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-15 13:32:15 -0300

    Criação do metodo de atualizar mensagem

--- scripts/petprofile.js
+++ scripts/petprofile.js
@@ -11,3 +11,46 @@ const messageArea = document.querySelector('#message_area');
 const message = document.querySelector('#message');
 const send = document.querySelector('#send');
 const update = document.querySelector('#update');
+
+function atualizaMensagens(){
+    var messageScroll = document.getElementById('message_area');
+    
+    messageArea.innerHTML = `
+        <div class="loading-area">
+            <p>Atualizando</p>
+            <img src="../images/loading.gif" alt="" class="loading-image">
+        </div>
+    `
+    fetch(`${BASE_URL_SERVER}${API_ANUNCIO}${id}`)
+        .then(res => res.json())
+        .then(anuncio => {
+            messageArea.innerHTML = ""
+            anuncio.mensagens.sort(function (a, b) {
+                if (a.idMensagem > b.idMensagem) {
+                  return 1;
+                }
+                if (a.idMensagem < b.idMensagem) {
+                  return -1;
+                }
+                return 0;
+              });
+
+            for(let mensagem of anuncio.mensagens){
+
+                const data = new Date(Date.parse(mensagem.dataMensagem))
+                let dataFormatada = adicionaZero((data.getDate())) + "." + ((data.getMonth() + 1)) + "." + data.getFullYear() + " - " + (data.getHours() + 3) + ":" + adicionaZero(data.getMinutes()); 
+
+                messageArea.innerHTML += 
+                `
+                    <p class="style-message">
+                    <span><strong>${mensagem.idPessoa.nome}:</strong></span>
+                    ${mensagem.mensagem}
+                    <span>${dataFormatada}</span>
+                    </p>
+                `
+            }
+        })
+        .then(() => {
+            messageScroll.scrollTop = messageScroll.scrollHeight - messageScroll.clientHeight;
+        })
+}
