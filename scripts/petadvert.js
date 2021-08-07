60ab816fbe865f02584b611f90a78170e95529e2
Mateus Araújo <mateusaraujo996@gmail.com>
2021-08-07 13:24:24 -0300

    Adcionando a funcionalidade de atualizar as mensagem do bate papo

--- scripts/petadvert.js
+++ scripts/petadvert.js
@@ -9,3 +9,46 @@ const castrado = document.querySelector("#castrado")
 const vacinado = document.querySelector("#vacinado")
 const nome = document.querySelector("#nome");
 const cep = document.querySelector("#cep");
+
+function atualizaMensagens(){
+    var messageScroll = document.getElementById('message_area');
+    
+    messageArea.innerHTML = `
+        <div class="loading-area">
+            <p>Atualizando</p>
+            <img src="../images/loading.gif" alt="" class="loading-image">
+        </div>
+    `
+    fetch(`${BASE_URL_SERVER}${API_ANUNCIO}${id}`)
+        .then(res => res.json())
+        .then(anuncio => {
+            messageArea.innerHTML = ""
+            anuncio.mensagens.sort(function (a, b) {
+                if (a.idMensagem > b.idMensagem) {
+                  return 1;
+                }
+                if (a.idMensagem < b.idMensagem) {
+                  return -1;
+                }
+                return 0;
+              });
+
+            for(let mensagem of anuncio.mensagens){
+
+                const data = new Date(Date.parse(mensagem.dataMensagem))
+                let dataFormatada = adicionaZero((data.getDate())) + "." + ((data.getMonth() + 1)) + "." + data.getFullYear() + " - " + (data.getHours() + 3) + ":" + adicionaZero(data.getMinutes()); 
+
+                messageArea.innerHTML += 
+                `
+                    <p class="style-message">
+                    <span><strong>${mensagem.idPessoa.nome}:</strong></span>
+                    ${mensagem.mensagem}
+                    <span>${dataFormatada}</span>
+                    </p>
+                `
+            }
+        })
+        .then(() => {
+            messageScroll.scrollTop = messageScroll.scrollHeight - messageScroll.clientHeight;
+        })
+}
